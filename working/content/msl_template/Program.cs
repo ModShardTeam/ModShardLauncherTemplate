d312f530b78ea08b416032d5d4148554edc9b8ca
remyCases <remy.cases@gmail.com>
2023-12-29 21:54:49 +0100

    Dotnet template config, to install it: dotnet new --install .\ in folder msl_template

---
+++ working/content/msl_template/Program.cs
@@ -0,0 +1,16 @@
+﻿using ModShardLauncher;
+using ModShardLauncher.Mods;
+
+namespace StoneShardMod;
+public class StoneShardMod : Mod
+{
+    public override string Author => "author";
+    public override string Name => "mod_name";
+    public override string Description => "mod_description";
+    public override string Version => "1.0.0.0";
+
+    public override void PatchMod()
+    {
+        
+    }
+}
